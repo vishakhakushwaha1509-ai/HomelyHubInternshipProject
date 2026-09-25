b3d461a19e2e890e2f55c97ecf4848eaf8294782
vishakhakushwaha1509-ai <vishakhakushwaha1509@gmail.com>
2026-09-25 12:14:31 +0530

    add deployed backend url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8080',
+        target: 'https://homelyhubinternshipproject-s2g6.onrender.com',
         changeOrigin: true,
         secure: false,
       }
